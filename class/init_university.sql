58312e96c330359f0895d3a9c5caa6584ab0de7a
yichuanshuzi <chenboshuo.hi@163.com>
2019-09-26 19:10:05 +0800

    :art: indent.规范缩进

--- class/init_university.sql
+++ class/init_university.sql
@@ -26,21 +26,37 @@ create table elective_course(
 );
 
 /* 录入数据 */
-insert into student values ('200215121', '李勇', '男', 20, 'CS');
-insert into student values ('200215122', '刘晨', '女', 19, 'CS');
-insert into student values ('200215123', '王敏', '女', 18, 'MA');
-insert into student values ('200215125', '张立', '男', 19, 'IS');
+insert into student
+	values ('200215121', '李勇', '男', 20, 'CS');
+insert into student
+	values ('200215122', '刘晨', '女', 19, 'CS');
+insert into student
+	values ('200215123', '王敏', '女', 18, 'MA');
+insert into student
+	values ('200215125', '张立', '男', 19, 'IS');
 
-insert into course values ('6', '数据处理', NULL, 2);
-insert into course values('2', '数学', NULL, 2);
-insert into course values ('4', '操作系统', '6', 3);
-insert into course values ('7', 'PASCAL语言', '6',4);
-insert into course values ('5', '数据结构', '7', 4);
-insert into course values('1', '数据库', '5', 4);
-insert into course values ('3', '信息系统', '1',4);
+insert into course
+	values ('6', '数据处理', NULL, 2);
+insert into course
+	values('2', '数学', NULL, 2);
+insert into course
+	values ('4', '操作系统', '6', 3);
+insert into course
+	values ('7', 'PASCAL语言', '6',4);
+insert into course
+	values ('5', '数据结构', '7', 4);
+insert into course
+	values('1', '数据库', '5', 4);
+insert into course
+	values ('3', '信息系统', '1',4);
 
-insert into elective_course values ('200215121', '1', 92);
-insert into elective_course values ('200215121', '2', 85);
-insert into elective_course values ('200215121', '3', 88);
-insert into elective_course values ('200215122', '2', 90);
-insert into elective_course values ('200215122', '3', 80);
+insert into elective_course
+	values ('200215121', '1', 92);
+insert into elective_course
+	values ('200215121', '2', 85);
+insert into elective_course
+	values ('200215121', '3', 88);
+insert into elective_course
+	values ('200215122', '2', 90);
+insert into elective_course
+	values ('200215122', '3', 80);
