8cd7cbd51c39914e11fa0cbfc9c29abd698cf855
yichuanshuzi <chenboshuo.hi@163.com>
2019-09-26 16:28:22 +0800

    :sparkles: add the insert of data. 增加了插入数据的语句

--- class/init_university.sql
+++ class/init_university.sql
@@ -24,3 +24,23 @@ create table elective_course(
 		/* 表级完整性约束条件, stu_id是外码, 被参照表是student */
 	foreign key (course_id) references course(course_id)
 );
+
+/* 录入数据 */
+insert into student values ('200215121', '李勇', '男', 20, 'CS');
+insert into student values ('200215122', '刘晨', '女', 19, 'CS');
+insert into student values ('200215123', '王敏', '女', 18, 'MA');
+insert into student values ('200215125', '张立', '男', 19, 'IS');
+
+insert into course values ('6', '数据处理', NULL, 2);
+insert into course values('2', '数学', NULL, 2);
+insert into course values ('4', '操作系统', '6', 3);
+insert into course values ('7', 'PASCAL语言', '6',4);
+insert into course values ('5', '数据结构', '7', 4);
+insert into course values('1', '数据库', '5', 4);
+insert into course values ('3', '信息系统', '1',4);
+
+insert into elective_course values ('200215121', '1', 92);
+insert into elective_course values ('200215121', '2', 85);
+insert into elective_course values ('200215121', '3', 88);
+insert into elective_course values ('200215122', '2', 90);
+insert into elective_course values ('200215122', '3', 80);
