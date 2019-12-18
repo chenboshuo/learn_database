61003393c8e8db3d061068e321d1208267894280
yichuanshuzi <chenboshuo.hi@163.com>
2019-12-18 20:32:08 +0800

    :sparkles: Grant to testuser. 向测试用户授权

--- final_project/test/testuser.sql
+++ final_project/test/testuser.sql
@@ -3,5 +3,9 @@ use mytimetable;
 -- 创建一个登录账号TestUser
 create login testuser with password = '123456';
 
--- 3．创建对应于这个登录账号的数据库用户TestUser
+-- 创建对应于这个登录账号的数据库用户TestUser
 create user testuser from login testuser ;
+
+-- 给用户授权
+grant select,execute,insert,alter  
+to testuser;
